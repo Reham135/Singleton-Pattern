5a24c86bdb3aa736b4769ec5f62b4e12296b8c86
Reham135 <reham7166@gmail.com>
2023-10-31 18:44:41 +0200

    Add project files.

---
+++ Traditional Singleton Pattern/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Traditional_Singleton_Pattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+         Singleton firstInstance=Singleton.GetInstance();
+         Singleton secondInstance=Singleton.GetInstance();
+         Console.WriteLine(firstInstance.GetHashCode());           //54267293
+         Console.WriteLine(secondInstance.GetHashCode());          //54267293
+         Console.WriteLine(firstInstance==secondInstance); //this will print true because they have the same hash code ,they isa the same obj
+
+        }
+    }
+}
---
+++ Traditional Singleton Pattern/Singleton.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Traditional_Singleton_Pattern;
+
+public class Singleton
+{
+    private static Singleton instance;      //It's declared as static to ensure there's only one instance shared across all instances of the class.
+
+    private Singleton()      //private ctor to prevent  the creation of new instances of the Singleton class from outside the class
+    {
+        
+    }
+
+
+    public static Singleton GetInstance()      //the method should be static to call it by class name without using any objects
+    { 
+        if (instance == null)
+        {
+            instance = new Singleton();
+        }
+        return instance;            // It first checks if instance is null, and if it is,
+                                    // it creates a new instance of the class and assigns it to instance.
+                                    // If an instance already exists, it returns the existing instance.
+    }
+
+
+}
